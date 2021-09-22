5a07eac2b6f069e9e5f30fec05483ecc0f26e700
jakob753951 <jako6440@edu.campusvejle.dk>
2021-09-22 17:21:57 +0200

    Print less >:[

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,8 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Some print");
-        System.out.println("Some more cool print");
         System.out.println("Even more print!");
     }
 }
