948037ee8761695d8c6db046835ea290fb9bbd68
vitus <jkvitus@hotmail.com>
2021-09-22 17:19:13 +0200

    Added more print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Some print");
-        System.out.println("Some more print");
+        System.out.println("Some more cool print");
+        System.out.println("Even more print!");
     }
 }
